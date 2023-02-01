7e6f7a0b6294578416dc7aa3c8b789754513b7b3
KonradRembisz <quake1528@gmail.com>
2023-02-01 12:23:14 +0100

    class

---
+++ sequence.h
@@ -0,0 +1,37 @@
+#ifndef __SEQUENCE_H__
+#define __SEQUENCE_H__
+
+#include <iostream>
+
+
+
+class Sequence{
+public:
+/**
+ * @brief Oblicza rozmycie Gaussa.
+ * 
+ * @details Rozmycie Gaussa jest jednym z popularnych filtrów obrazów, którego celem jest usuwanie szumów. 
+ * Wzór na rozmycie Gaussa jest następujący:
+ * 
+ * @f$ G(x, y) = \frac{1}{2 \pi \sigma^2} e^{-(x^2 + y^2) / 2 \sigma^2} @f$
+ * 
+ * gdzie @f$ x @f$ i @f$ y @f$ oznaczają współrzędne punktu w obrazie, a @f$ \sigma @f$ jest stałą określającą stopień rozmycia.
+ * 
+ * @param x Współrzędna @f$ x @f$ punktu w obrazie.
+ * @param y Współrzędna @f$ y @f$ punktu w obrazie.
+ * @param sigma Stała określająca stopień rozmycia.
+ * 
+ * @return Wartość rozmycia Gaussa dla danego punktu w obrazie.
+ */
+    Sequence(int x, int y);
+    Sequence(Sequence kopia);
+    ~Sequence();
+    int accessX();
+    int accessY();
+    float dodaj();
+    float odejmij();
+private:
+    int x, y;
+}
+
+#endif
---
+++ statistics.h
@@ -0,0 +1,32 @@
+#ifndef __STATISTICS_H__
+#define __STATISTICS_H__
+
+#include <iostream>
+
+
+
+class Statistics{
+public:
+//! \brief Inny przykładowy wzór:
+//! \f[
+//!    |I_2|=\left| \int_{0}^T \psi(t) 
+//!             \left\{ 
+//!                u(a,t)-
+//!                \int_{\gamma(t)}^a 
+//!                \frac{d\theta}{k(\theta,t)}
+//!                \int_{a}^\theta c(\xi)u_t(\xi,t)\,d\xi
+//!             \right\} dt
+//!          \right|
+//!  \f]
+    Statistics(int x, int y);
+    Statistics(Statistics kopia);
+    ~Statistics();
+    int accessX();
+    int accessY();
+    float dodaj();
+    float odejmij();
+private:
+    int x, y;
+}
+
+#endif
