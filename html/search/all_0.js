9ae408eb03e2f5cce8b2cd6cc16c4375b3341c8d
KonradRembisz <quake1528@gmail.com>
2023-02-01 12:57:06 +0100

    doxpage

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['konrad_5frembisz_0',['Konrad_Rembisz',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ html/search/all_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['sequence_0',['Sequence',['../class_sequence.html',1,'Sequence'],['../class_sequence.html#afb5b597d203629635ebe96b335ba9d5a',1,'Sequence::Sequence()']]],
+  ['statistics_1',['Statistics',['../class_statistics.html',1,'Statistics'],['../class_statistics.html#a94073a0d8a99ca81fc8cdc7cceef9ee1',1,'Statistics::Statistics()']]],
+  ['strona_20z_20obrazkiem_2',['Strona z obrazkiem',['../page1.html',1,'index']]],
+  ['strona_20z_20wykresem_3',['Strona z wykresem',['../page2.html',1,'']]]
+];
